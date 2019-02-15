c53859dcec3ce1bf332d0bb898c3ddfa531ffbd6
Marwan <zouinkhi.marwan@gmail.com>
2019-02-15 17:08:03 +0100

    start project

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,21 @@
 package main;
 
+import java.io.File;
+
+import net.imglib2.FinalDimensions;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.real.FloatType;
+import tools.IOFunctions;
+
 public class Main {
 
+	private static final String lsmPath = "";
+	private static final String flyPath = "img/fly.tif";
+	private static final String distributionPath = "";
+	
 	
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("img/fly.tif");
+		Img<FloatType> image = IOFunctions.openAs32Bit(new File(flyPath));
 	}
 }
---
+++ src/tools/MyCallBack.java
@@ -0,0 +1,8 @@
+package tools;
+
+public interface MyCallBack {
+	void onSuccess();
+	void onError(String error);
+	void log(String log);
+}
+
